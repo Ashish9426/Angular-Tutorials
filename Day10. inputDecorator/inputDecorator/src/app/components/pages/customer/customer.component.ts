eed14712cb943b4036981a5f494043b1617e397b
Ashish Mishra <ashishmishra9426@gmail.com>
2021-10-25 10:12:45 +0530

    update

--- Day10. inputDecorator/inputDecorator/src/app/components/pages/customer/customer.component.ts
+++ Day10. inputDecorator/inputDecorator/src/app/components/pages/customer/customer.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
   @Input() msg:any;
-
   @Output() message=new EventEmitter();
-  constructor() { }
+
+  employee:any
+
+  constructor() { 
+    this.employee = [
+      {'id':1,'name':'ajay','gender':1,'salary':13000,'dob':new Date('1997-08-3')},
+      {'id':2,'name':'vijay','gender':1,'salary':18000,'dob':new Date('1998-08-3')},
+      {'id':3,'name':'rahul','gender':1,'salary':16000,'dob':new Date('1994-05-3')},
+      {'id':4,'name':'amit','gender':1,'salary':13900,'dob':new Date('1991-02-3')},
+      {'id':5,'name':'riya','gender':0,'salary':14000,'dob':new Date('1995-09-3')}
+    ]
+  }
 
   ngOnInit(): void {
   }
